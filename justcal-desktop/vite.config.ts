966c977c001e1543c1377ddb6450c48c2d0dbbd3
Joseph Cherian <41300035+JCherry101@users.noreply.github.com>
2026-03-06 17:35:41 +0400

    new updates

--- justcal-desktop/vite.config.ts
+++ justcal-desktop/vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
+  plugins: [react(), tailwindcss()],
+  
+  // VITE RUST SERVER SETTINGS BELOW
+  // Do not delete the clearScreen, server, or envPrefix settings!
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
